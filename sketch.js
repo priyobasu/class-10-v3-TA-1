e459e6402698e4ac31c09a3ab7b1f6aea753130c
priyobasu <72121579+priyobasu@users.noreply.github.com>
2021-10-01 07:09:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var marks=[30,35,45,38,40];
+console.log(marks);
+console.log(marks[0]);
+
+function score_average(){
+  var sum=marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
+  var avg=sum/marks.length;
+  console.log(avg);
+}
+
+function setup() {
+  createCanvas(400, 400);
+  score_average();
+}
+
+function draw() {
+  background(220);
+}
